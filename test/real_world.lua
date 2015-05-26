84bd2c2dbb9dc5905aecfcdfe7d1e1c611539af2
Florian Mayer <mayer.florian@web.de>
2015-05-26 03:19:38 +0200

    Added new test

---
+++ test/real_world.lua
@@ -0,0 +1,46 @@
+local h = loadfile("../h5tk.lua")().init(true)
+
+io.write("<!DOCTYPE html>")
+io.write(h.emit(h.html{
+	h.head{
+		h.title{ "This is a nice title!" },
+		h.link{rel="icon", href="foo", type="image/vnd.microsoft.icon"},
+		h.link{rel="stylesheet", href="/style.css", type="text/css"},
+		h.meta{charset="utf-8"}
+	}, 
+	h.body{
+		h.h1{"This is a level 1 heading"},
+		h.h2{"some foo"},
+		h.p{"blablabla"},
+		h.h2{"some morefoo"},
+		
+		h.form{
+			action="foo.lua",
+			{h.input{type="radio", name="cb1", unchecked=""}, "label foo cb1"},
+			{h.input{type="radio", name="cb2", unchecked=""}, "label foo cb2"},
+			{h.input{type="text", name="s"}, "label foo text"},
+			{h.input{type="submit", value="bam"}, "label bam"}
+		},
+
+		h.h1{"this is very cool. Look!!!"},
+		h.pre{
+			"foo"
+		},
+
+		h.h1{"A list"},
+		h.ul{
+			(function()
+				local tmp = {}
+				for i=1,10 do
+					tmp[i] = h.li{
+						h.a{
+							href="http://static-link.com/foo"..i..".png", 
+							"look at my " .. tostring(i).."-th picture"
+						}
+					}
+				end
+				return tmp
+			end)
+		}
+	}
+}))
