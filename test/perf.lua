13dfd58247f236f360be89fa55f48a3de845dd58
Florian Mayer <mayer.florian@web.de>
2015-05-26 02:41:30 +0200

    Added a new test

---
+++ test/perf.lua
@@ -0,0 +1,31 @@
+local h5tk = loadfile("../h5tk.lua")().init(false)
+
+
+for i=1,1000 do
+h5tk.emit(h5tk.html{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+	h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{h5tk.table{
+
+	} } } } } } } } } } } } } } } } } } } } } }
+	} } } } } } } } } } } } } } } } } } } } } }
+	} } } } } } } } } } } } } } } } } } } } } }
+	} } } } } }  
+
+	} } } } } } } } } } } } } } } } } } } } } }
+	} } } } } } } } } } } } } } } } } } } } } }
+})
+end
